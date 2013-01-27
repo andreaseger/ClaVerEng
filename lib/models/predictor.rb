3fb542bfc6cf07802993ca709a52df512691b11d
Andreas Eger <dev@eger-andreas.de>
2013-01-28 00:53:55 +0100

    remove pry line, update svm_helper

--- lib/models/predictor.rb
+++ lib/models/predictor.rb
@@ -63,7 +63,6 @@ class Predictor  < ActiveRecord::Base
   private
   def serialize_model
     self.serialized_model = model.serialize
-    binding.pry
   end
   def update_settings
     self.used_preprocessor ||= preprocessor.class.to_s
