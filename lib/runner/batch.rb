824fcd2e7abd0c6b8b4f619ef87f28d3f55db7e6
Andreas Eger <dev@eger-andreas.de>
2013-01-31 12:03:31 +0100

    refactoring runner

---
+++ lib/runner/batch.rb
@@ -0,0 +1,8 @@
+class BatchRunner
+  def initialize(args)
+  end
+  
+  def feature_vectors(size, classification, language=6)
+    
+  end
+end
--- lib/runner/individual.rb
+++ lib/runner/individual.rb
@@ -1,9 +1,10 @@
+require_relative 'base'
 #
 # Runs different SVM training setups
 #
 # @author Andreas Eger
 #
-class Runner
+class Individual < Base
   include SvmTrainer
   attr_accessor :preprocessor
   attr_accessor :selector
@@ -67,39 +68,6 @@ class Runner
     predictor
   end
 
-
-  #
-  # fetch job data with a 50/50 distribution between correct and false classification
-  # @param  classification [Symbol] in `:industry`, `:function`, `:career_level`
-  # @param  offset [Integer] Offset for the database queries
-  #
-  # @return [Array<PreprocessedData>]
-  def fetch_and_preprocess classification, offset=nil
-    jobs = fetch_jobs(classification, 6, @max_samplesize/2, offset)
-    @preprocessor.process(jobs, classification)
-  end
-
-
-  #
-  # fetch, preprocess a test set, generate feature vectors and create a libsvm Problem
-  # @param  classification [Symbol] in `:industry`, `:function`, `:career_level`
-  #
-  # @return [Problem] libsvm Problem
-  def fetch_test_set classification
-    data = @preprocessor.process(fetch_jobs(classification), classification)
-    set = @selector.generate_vectors(data, classification, @dictionary_size)
-    Libsvm::Problem.new.tap{|p| p.set_examples(set.map(&:label), set.map{|e| Libsvm::Node.features(e.data)})}
-  end
-
-  def fetch_jobs(classification, language = 6, limit = nil, offset = nil)
-    faulty = Job.with_language(language).faulty_for_classification(classification)
-    faulty = faulty.limit(limit) if limit
-    faulty = faulty.offset(offset) if offset
-
-    correct =  Job.with_language(language).correct_for_classification(classification).limit(faulty.size)
-    correct = correct.offset(offset) if offset
-    faulty + correct
-  end
   private
   def setup args
     folds = args.fetch(:folds) { 3 }
